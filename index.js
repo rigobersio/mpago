4910d7ec2c640b9b642a6089eb3c6bc0f3033292
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-05-28 16:46:23 -0400

    (rigoberto)edit:index(elimino el bloque restante de base de datos)

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const axios = require("axios");
 const server = require("./src/server");
-const { conn } = require('./src/db.js');
 const PORT = 3000;
 
-conn.sync({ force: true }).then(() => { 
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
-})
-}).catch(error => console.error(error))
-//force es importante sin force hay error; false no reinicia la base de datos
+});
